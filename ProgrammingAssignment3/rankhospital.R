c7b936e00e0d0998f0354b6f10807cec6944708c
Lindsey Handley <lindseyhandley@gmail.com>
2018-01-06 12:57:08 -0800

    refactored rankhospital.R

--- ProgrammingAssignment3/rankhospital.R
+++ ProgrammingAssignment3/rankhospital.R
@@ -1,4 +1,36 @@
 ##Same as best() function, but it can give whatever rank, not just "best"
+  ##Reads in data
+  ##Makes sure state & outcome are valid
+  ##Then for each outcome:
+    ##Extracts outcome data from original data that is from that state
+    ##Removes NA data
+    ##Sorts state data and uses the num to return either the best, worst, or whatever ranking specified
+
+findRankFromSortedStatesData <- function(ds,num){
+  length <- nrow(ds) ##Stores length (# of rows) of data frame for if statements below
+  
+  if(num == "best"){
+    ds[1,2] ##Returns hospital name at top of dataframe
+  }
+  else if(num == "worst"){
+    ds[length,2] ##Returns hospital at bottom of dataframe
+  }
+  else if(num > length){
+    return(NA) ##There is no specified ranking that large
+  }
+  else{
+    ds[num,2] ##Returns hospital name at top of dataframe
+  }
+}
+
+prepareData <- function(ds, colnum, st){
+  states_data <- ds[ds$State == st,] ##Extracts rows of hospitals in specified state
+  states_data[, colnum] <- as.numeric(states_data[,colnum]) ##Sets column 11 to be numeric instead of character
+  good_states_data <- complete.cases(states_data[,colnum])  ##Finds rows containing NA in col 11
+  states_data <- states_data[good_states_data,] ##Updates states_data to remove NA rows
+  states_data <- states_data[order(states_data[,colnum],states_data[,2]),] ##Sorts lowest -> highest all 30-day mortality data (col 11), for ties sorts hospital name (col 2)
+  states_data
+}
 
 rankhospital <- function(state, outcome, num = "best") {
   all_data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
@@ -10,71 +42,27 @@ rankhospital <- function(state, outcome, num = "best") {
   ##HEART ATTACK
   ##Column w/ 30-day mortality for heart attack is 11
   if(outcome == "heart attack"){
-    states_data <- all_data[all_data$State == state,] ##Extracts rows of hospitals in specified state
-    states_data[, 11] <- as.numeric(states_data[, 11]) ##Sets column 11 to be numeric instead of character
-    good_states_data <- complete.cases(states_data[,11])  ##Finds rows containing NA in col 11
-    states_data <- states_data[good_states_data,] ##Updates states_data to remove NA rows
-    states_data <- states_data[order(states_data[,11],states_data[,2]),] ##Sorts lowest -> highest all 30-day mortality data (col 11), for ties sorts hospital name (col 2)
-    length <- nrow(states_data) ##Stores length (# of rows) of data frame for if statements below
-    if(num == "best"){
-      states_data[1,2] ##Returns hospital name at top of dataframe
-    }
-    else if(num == "worst"){
-      states_data[length,2] ##Returns hospital at bottom of dataframe
-    }
-    else if(num > length){
-      return(NA) ##There is no specified ranking that large
-    }
-    else{
-      states_data[num,2] ##Returns hospital name at top of dataframe
-    }
+    states_data <- prepareData(all_data,11,state)
+    
+    findRankFromSortedStatesData(states_data,num)
   }
   
   ##HEART FAILURE
   ##Column w/ 30-day mortality for heart failure is 17
   else if(outcome == "heart failure"){
-    states_data <- all_data[all_data$State == state,] ##Extracts rows of hospitals in specified state
-    states_data[, 17] <- as.numeric(states_data[, 17]) ##Sets column 11 to be numeric instead of character
-    good_states_data <- complete.cases(states_data[,17])  ##Finds rows containing NA in col 11
-    states_data <- states_data[good_states_data,] ##Updates states_data to remove NA rows
-    states_data <- states_data[order(states_data[,17],states_data[,2]),] ##Sorts lowest -> highest all 30-day mortality data (col 11), for ties sorts hospital name (col 2)
-    length <- nrow(states_data) ##Stores length (# of rows) of data frame for if statements below
-    if(num == "best"){
-      states_data[1,2] ##Returns hospital name at top of dataframe
-    }
-    else if(num == "worst"){
-      states_data[length,2] ##Returns hospital at bottom of dataframe
-    }
-    else if(num > length){
-      return(NA) ##There is no specified ranking that large
-    }
-    else{
-      states_data[num,2] ##Returns hospital name at top of dataframe
-    }
+    states_data <- prepareData(all_data,17,state)
+    
+    findRankFromSortedStatesData(states_data,num)
   }
   
   ##PNEUMONIA
   ##Column w/ 30-day mortality for pneumonia is 23
   else if(outcome == "pneumonia"){
-    states_data <- all_data[all_data$State == state,] ##Extracts rows of hospitals in specified state
-    states_data[, 23] <- as.numeric(states_data[, 23]) ##Sets column 11 to be numeric instead of character
-    good_states_data <- complete.cases(states_data[,23])  ##Finds rows containing NA in col 11
-    states_data <- states_data[good_states_data,] ##Updates states_data to remove NA rows
-    states_data <- states_data[order(states_data[,23],states_data[,2]),] ##Sorts lowest -> highest all 30-day mortality data (col 11), for ties sorts hospital name (col 2)
-    length <- nrow(states_data) ##Stores length (# of rows) of data frame for if statements below
-    if(num == "best"){
-      states_data[1,2] ##Returns hospital name at top of dataframe
-    }
-    else if(num == "worst"){
-      states_data[length,2] ##Returns hospital at bottom of dataframe
-    }
-    else if(num > length){
-      return(NA) ##There is no specified ranking that large
-    }
-    else{
-      states_data[num,2] ##Returns hospital name at top of dataframe
-    }
+    states_data <- prepareData(all_data,23,state)
+    
+    findRankFromSortedStatesData(states_data,num)
   }
+  
   else{
     stop("invalid outcome")   ##OUTCOME ARGUMENT DOESN'T MAKE SENSE
   }
